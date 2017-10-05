5833d87a0284a2ae6678f4c1fc1c70d6fa3c8ec2
Joel Dreise <jdreise@localhost.localdomain>
2017-10-05 19:37:53 +0200

    Changed routes to make them more user friendly

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,17 @@
 Auth::routes();
 
 Route::get('/', 'ChapterController@index')->name('chapter.index');
-Route::get('s/', 'SeriesController@index')->name('series.index');
-Route::get('s/{seriesSlug}', 'SeriesController@show');
-Route::get('s/{seriesSlug}/{bookSlug}', 'BookController@show');
-Route::get('s/{seriesSlug}/{bookSlug}/{chapterNumber}', 'ChapterController@show');
+Route::get('series/', 'SeriesController@index')->name('series.index');
+Route::get('series/{seriesSlug}', 'SeriesController@show');
+Route::get('series/{seriesSlug}/{bookSlug}', 'BookController@show');
+Route::get('series/{seriesSlug}/{bookSlug}/{chapterNumber}', 'ChapterController@show');
 
 Route::group(['middleware' => 'auth'], function()
 {
-    Route::get('series/create', 'SeriesController@create')->name('series.create');
-    Route::post('series/store', 'SeriesController@store')->name('series.store');
-    Route::get('series/{seriesSlug}/create', 'BookController@create');
-    Route::post('series/{seriesSlug}/store', 'BookController@store');    
-    Route::get('series/{seriesSlug}/book/{bookSlug}/create', 'ChapterController@create');
-    Route::post('series/{seriesSlug}/book/{bookSlug}/store', 'ChapterController@store');
-
+    Route::get('creator-hub/create', 'SeriesController@create')->name('series.create');
+    Route::post('creator-hub/store', 'SeriesController@store')->name('series.store');
+    Route::get('creator-hub/{seriesSlug}/create', 'BookController@create');
+    Route::post('creator-hub/{seriesSlug}/store', 'BookController@store');
+    Route::get('creator-hub/{seriesSlug}/{bookSlug}/create', 'ChapterController@create');
+    Route::post('creator-hub/{seriesSlug}/{bookSlug}/store', 'ChapterController@store');
 });
