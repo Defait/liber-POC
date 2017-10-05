ada2e4dab3166f6f776f6e1e800ec7fafde86b85
Joel Dreise <jdreise@localhost.localdomain>
2017-10-05 22:38:11 +0200

    forgot to have the cover link to the series in series.index

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,11 +7,13 @@
             @foreach($chunks as $serie)        
                 <div class="col s12 m6">
                     <div class="card">
-                        <div class="card-image">
-                            <img src="{{ asset($serie->information->cover_img_location) }}">
-                            <span class="card-title">{{$serie->name}}</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red" href="series/{{$serie->slug}}"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                        </div>
+                        <a href="series/{{$serie->slug}}">
+                            <div class="card-image">
+                                <img src="{{ asset($serie->information->cover_img_location) }}">
+                                <span class="card-title">{{$serie->name}}</span>
+                                <a class="btn-floating halfway-fab waves-effect waves-light red" href="series/{{$serie->slug}}"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                            </div>
+                        </a>
                         <div class="card-content">
                             <p>{{str_limit($serie->information->synopsis, 300)}}</p>
                         </div>
